5a812eadfd7e118286bc95108ca46047b59d9da4
makthakur87 <thakurr.mandeep@gmail.com>
2018-08-12 09:16:58 -0400

    trigger added

---
+++ Trigger.bat
@@ -0,0 +1,4 @@
+cd C:\Selenium_Workspace\TestNGFramework
+dir
+set classpath=C:\Selenium_Workspace\TestNGFramework\bin;C:\Selenium_Workspace\TestNGFramework\lib\*
+java org.testng.TestNG C:\Selenium_Workspace\TestNGFramework\testng.xml
